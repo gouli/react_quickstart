6ad0fe802747306e6f453f8874536123f65785aa
Mahesha Gouli <gouli@Jeffs-MacBook-Pro-2.local>
2018-06-11 13:22:07 -0400

    Initial commit

---
+++ src/Plugin/Block/ClockFaceBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\react_quickstart\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Clock Face' Block
+ *
+ * @Block(
+ *   id = "clock_face",
+ *   admin_label = @Translation("Clock Face Block"),
+ * )
+ */
+class ClockFaceBlock extends BlockBase {
+    /**
+     * {@inheritdoc}
+     */
+    public function build() {
+        return array(
+            '#type' => 'markup',
+            '#markup' => "<div id='clock'></div>",
+            '#attached' => array(
+                'library' =>  array(
+                    'react_quickstart/clock'
+                ),
+            ),
+        );
+    }
+}
+?>
